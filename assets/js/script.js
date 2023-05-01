f1ac2198c660eec9e90cf37d20db4aff8215f559
Emma Karlsson <emma94k@gmail.com>
2023-05-01 19:01:07 +0000

    Add code to generate 5 random numbers and returning name

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,6 +22,21 @@ function runGame() {
 }
 
 function computerTurn() {
+
+    let randNum = Math.floor(Math.random() * 5) + 1;
+
+    switch(randNum){
+      case 1:
+        return "Rock";
+      case 2:
+        return "Paper";
+      case 3:
+        return "Scissors";
+      case 4:
+        return "Lizard";
+      case 5:
+        return "Spock";
+    }
 }
 
 function checkWinner() {
