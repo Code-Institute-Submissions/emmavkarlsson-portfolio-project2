be277f2ecdb994e42bc18ed312177e7bdb49cb53
Emma Karlsson <emma94k@gmail.com>
2023-05-01 18:59:41 +0000

    Add event listener for document loading and function for button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
+document.addEventListener("DOMContentLoaded", initializeGame);
+
+let buttons = document.getElementsByClassName('btn-selection');
+
+function initializeGame(event) {
+    for (let button of buttons) {
+        const userChoice = button.name;
+        
+        button.addEventListener('click', function(event) {
+            makeUserSelection(userChoice);
+        });
+    }
+}
+
 function toggleButtons() {
 }
 
