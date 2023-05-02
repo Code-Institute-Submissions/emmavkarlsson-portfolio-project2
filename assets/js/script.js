49a12e1e2dd38644fa2f90d77840df75bf9baba2
Emma Karlsson <emma94k@gmail.com>
2023-05-02 06:45:17 +0000

    Update equality to strict equality and set default for switch case

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,4 @@
-document.addEventListener("DOMContentLoaded", initializeGame);
 
-let buttons = document.getElementsByClassName('btn-selection');
-
-function initializeGame(event) {
-    for (let button of buttons) {
-        let userChoice = button.name;
-        
-        button.addEventListener('click', function(event) {
-            makeUserSelection(userChoice);
-        });
-    }
-}
 
 let user = document.getElementById('users-choice');
 let computer = document.getElementById('computers-choice');
@@ -20,12 +8,25 @@ let messageSpan = document.getElementById('message');
 let winsLabel = document.getElementById('wins');
 let tiesLabel = document.getElementById('ties');
 let lossesLabel = document.getElementById('losses');
+let buttons = document.getElementsByClassName('btn-selection');
 
 // Game variables
-let gameInProgress = false;
 let userScore = 0;
 let computerScore = 0;
 let tieGames = 0;
+
+/**
+ * Make user selection based on the button it clicks
+*/
+function initializeGame(event) {
+    for (let button of buttons) {
+        let userChoice = button.name;
+        
+        button.addEventListener('click', function(event) {
+            makeUserSelection(userChoice);
+        });
+    }
+}
 
 /**
  * Disabling the buttons when the user has clicked on them,
@@ -44,13 +45,9 @@ function toggleButtons(isEnabled) {
 
 /**
  * The game function for when the user has made it's choice
+ * takes in the userChoice parameter
  */
 function makeUserSelection(userChoice) {
-    if (gameInProgress == true) {
-        return;
-    }
-
-    gameInProgress = true;
     toggleButtons(false);
 
     messageSpan.textContent = "Please wait for computer to select";
@@ -60,7 +57,6 @@ function makeUserSelection(userChoice) {
 
     setTimeout(function() {
         runGame(userChoice);
-        gameInProgress = false;
         toggleButtons(true);
     }, 1500);    
 }
@@ -89,67 +85,62 @@ function runGame(userChoice) {
  * and returning names based on these
  */
 function computerTurn() {
-
+    
     let randNum = Math.floor(Math.random() * 5) + 1;
 
     switch(randNum){
-      case 1:
-        return "Rock";
-      case 2:
-        return "Paper";
-      case 3:
-        return "Scissors";
-      case 4:
-        return "Lizard";
-      case 5:
-        return "Spock";
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+        case 4:
+            return "Lizard";
+        case 5:
+            return "Spock";
+        default:
+            return "Error";
     }
 }
 
 /**
- * Checks the winner of the game
+ * Checks the winner of the game based on the parameters userChoice and computerChoice
  * 0 = tie
  * 1 = user wins
  * -1 = computer wins
  */
 function checkWinner(userChoice, computerChoice) {
-    if(userChoice == computerChoice){
+    if(userChoice === computerChoice){
       return 0;
-    } 
-    
-    if (computerChoice == "Rock") {
-        return (userChoice == "Paper" || userChoice == "Spock") ? 1 : -1;
-    } 
-    
-    if (computerChoice == "Paper") {
-        return (userChoice == "Scissors" || userChoice == "Lizard") ? 1 : -1;
-    } 
-    
-    if (computerChoice == "Scissors") {
-        return (userChoice == "Rock" || userChoice == "Spock") ? 1 : -1;
-    } 
-    
-    if (computerChoice == "Lizard") {
-        return (userChoice == "Rock" || userChoice == "Scissors") ? 1 : -1;
-    } 
-    
-    if (computerChoice == "Spock") {
-        return (userChoice == "Lizard" || userChoice == "Paper") ? 1 : -1;
+    } else if (computerChoice === "Rock") {
+        return (userChoice === "Paper" || userChoice === "Spock") ? 1 : -1;
+    } else if (computerChoice === "Paper") {
+        return (userChoice === "Scissors" || userChoice === "Lizard") ? 1 : -1;
+    } else if (computerChoice === "Scissors") {
+        return (userChoice == "Rock" || userChoice === "Spock") ? 1 : -1;
+    } else if (computerChoice === "Lizard") {
+        return (userChoice === "Rock" || userChoice === "Scissors") ? 1 : -1;
+    } else if (computerChoice === "Spock") {
+        return (userChoice === "Lizard" || userChoice === "Paper") ? 1 : -1;
     }
 }
 
 /**
  * Increments the scores and records the results
+ * takes in the parameter result
  */
 function recordResult(result) {
-    if (result == 1) {
+    if (result === 1) {
         userScore++;
         return "You Win!";
-    } else if (result == 0) {
+    } else if (result === 0) {
         tieGames++;
         return "It's a tie!";
-    } else if (result == -1) {
+    } else if (result === -1) {
         computerScore++;
         return "You Lose!";
     }
 }
+
+document.addEventListener("DOMContentLoaded", initializeGame);
